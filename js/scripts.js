1f7eb71900eaca4b51d2fbe85384338ebccf5ad2
Shatha Shaath and Georgina Van Dort <georgina@>
2016-05-31 14:45:16 -0700

    added flashcards

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    $("button#javascriptbutton").click(function(){
+        $("p#java-script-definition").slideToggle();
+    });
+
+    $("button#Operators").click(function(){
+        $("p#Operators-def").slideToggle();
+    });
+
+    $("button#Variables").click(function(){
+        $("p#Variables-def").slideToggle();
+    });
+
+
+    $("button#Variable naming conventions").click(function(){
+        $("p#Variable naming conventions-def").slideToggle();
+    });
+
+    $("button#Functions").click(function(){
+        $("p#Functions-def").slideToggle();
+    });
+
+    $("button#Methods").click(function(){
+        $("p#Methods-def").slideToggle();
+    });
+
+    $("button#Arguments").click(function(){
+        $("p#Arguments-def").slideToggle();
+    });
+
+    $("button#Parameters").click(function(){
+        $("p#Parameters-def").slideToggle();
+    });
+
+    $("button#Functions").click(function(){
+        $("p#Functions-def").slideToggle();
+    });
+
+    $("button#Return").click(function(){
+        $("p#Return-def").slideToggle();
+    });
+
+    $("button#Strings").click(function(){
+        $("p#Strings-def").slideToggle();
+    });
+
+    $("button#Booleans").click(function(){
+        $("p#Booleans-def").slideToggle();
+    });
+
+});
